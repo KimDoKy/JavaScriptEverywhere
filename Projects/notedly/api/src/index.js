be498bb151db2774739141d69baac08d3a01c744
KimDoKy <makingfunk0@gmail.com>
2021-02-04 21:00:52 +0900

    chap7. 사용자 계정과 인증

--- Projects/notedly/api/src/index.js
+++ Projects/notedly/api/src/index.js
@@ -7,6 +7,20 @@ const db = require('./db');
 const models = require('./models');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
+const jwt = require('jsonwebtoken');
+
+// JWT에서 사용자 정보 가져오기
+const getUser = token => {
+    if (token) {
+        try {
+            // 토큰에서 얻은 사용자 정보 반환
+            return jwt.verify(token, process.env.JWT_SECRET);
+        } catch (err) {
+            // 토큰에 문제가 있으면 에러 던지기
+            throw new Error('Session invalid');
+        }
+    }
+};
 
 // .env 파일에 명시된 포트 또는 포트 4000에서 서버를 실행
 const port = process.env.PORT || 4000;
@@ -20,8 +34,14 @@ db.connect(DB_HOST);
 const server = new ApolloServer({ 
     typeDefs, 
     resolvers,
-    context: () => {
-        // context에 db models 추가
+    context: ({ req }) => {
+        // 헤더에서 사용자 토큰 가져오기
+        const token = req.headers.authorization;
+        // 토큰에서 사용자 얻기
+        const user = getUser(token);
+        // 콘솔에 user 로깅
+        console.log(user);
+        // context에 db models 및 user 추가
         return { models };
     }
 });
