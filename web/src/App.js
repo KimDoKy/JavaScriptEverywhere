f12f606cf4b34696c42c0c69be3ca150c4e1469f
KimDoKy <makingfunk0@gmail.com>
2021-02-07 14:23:55 +0900

    chap12. 리액트로 웹 클라이언트 만들기

---
+++ web/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import Pages from './pages';
+
+const App = () => {
+    return (
+        <div>
+          <Pages />
+        </div>
+    );
+};
+
+ReactDom.render(<App />, document.getElementById('root'));
