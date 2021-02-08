68b9a8b82102d01ae92465d624c28fe9b908931a
KimDoKy <makingfunk0@gmail.com>
2021-02-08 20:14:58 +0900

    chap14. 아폴로 클라이언트로 작업하기 / 14.1 아폴로 클라이언트 셋업

--- web/src/App.js
+++ web/src/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-// 글로벌 스타일 임포트
-import GlobalStyle from './components/GlobalStyle';
+// 아폴로 클라이언트 라이브러리 임포트
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
+import GlobalStyle from './components/GlobalStyle';
 import Pages from './pages';
+
+// API URI 및 캐시 설정
+const uri = process.env.API_URI;
+const cache = new InMemoryCache();
+
+// 아폴로 클라이언트 설정
+const client = new ApolloClient({
+    uri,
+    cache,
+    connectToDevTools: true
+});
 
 const App = () => {
     return (
-        <div>
+        <ApolloProvider client={client}>
           <GlobalStyle />
           <Pages />
-        </div>
+        </ApolloProvider>
     );
 };
 
