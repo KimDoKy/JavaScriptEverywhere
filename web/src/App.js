e21e10e933bd6b464434edd03f5eede21b7f4d75
KimDoKy <makingfunk0@gmail.com>
2021-02-09 20:00:34 +0900

    chap15 / 15.3 요청에 헤더 붙이기

--- web/src/App.js
+++ web/src/App.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-// 아폴로 클라이언트 라이브러리 임포트
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { 
+    ApolloClient, 
+    ApolloProvider, 
+    createHttpLink,
+    InMemoryCache 
+} from '@apollo/client';
+import { setContext } from 'apollo-link-context';
 
 import GlobalStyle from './components/GlobalStyle';
 import Pages from './pages';
 
 // API URI 및 캐시 설정
 const uri = process.env.API_URI;
+const httpLink = createHttpLink({ uri });
 const cache = new InMemoryCache();
+
+// 토큰을 확인하고 컨텍스트에 대한 헤더 반환
+const authLink = setContext((_, { headers }) => {
+    return {
+        headers: {
+            ...headers,
+            authorization: localStorage.getItem('token') || ''
+        }
+    };
+});
 
 // 아폴로 클라이언트 설정
 const client = new ApolloClient({
-    uri,
+    link: authLink.concat(httpLink),
     cache,
+    resolvers: {},
     connectToDevTools: true
 });
 
