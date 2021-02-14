8df83bc983d85a3931993f2ca893294b7d30d96f
KimDoKy <makingfunk0@gmail.com>
2021-02-14 20:38:39 +0900

    chap19 19.2 설정 19.3 콘텐츠 보안 정책

--- desktop/src/index.js
+++ desktop/src/index.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require('electron');
-const { is } = require('electron-util');
+const { is, setContentSecurityPolicy } = require('electron-util');
+const config = require('./config');
 
 // 가비지 콜렉션을 피하기 위해, 윈도우를 변수로 선언
 let window;
@@ -14,12 +15,32 @@ function createWindow() {
         }
     });
 
-    // HTML 파일 불러오기
-    window.loadURL('http://localhost:1234');
+    // URL 불러오기
+    if (is.development) {
+        window.loadURL(config.LOCAL_WEB_URL);
+    } else {
+        window.loadURL(confi.PRODUCTION_WEB_URL);
+    }
+
     // 개발자 모드라면 브라우저 개발자 도구 열기
     if (is.development) {
         window.webContents.openDevTools();
     }
+
+    // 프로덕션 모드에서 CSP 설정
+    if (!is.development) {
+        setContentSecurityPolicy(`
+            default-src 'none';
+            script-src 'self';
+            img-src 'self' https://www.gravatar.com'
+            style-src 'self';
+            connect-src 'self' ${config.PRODUCTION_API_URL};
+            base-uri 'none';
+            form-action 'none';
+            frame-ancestors 'none';
+        `);
+    }
+
     // 윈도우가 닫히면 윈도우 객체 초기화
     window.on('closed', () => {
         window = null;
